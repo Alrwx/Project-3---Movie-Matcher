1136536515fd8247b58bcec86cd0654e15f7d29e
Alvin Cabe <jackcabe123@gmail.com>
2025-08-02 04:07:33 -0400

    added testing cpp file for testing certain inputs

--- main.cpp
+++ main.cpp
@@ -1,60 +1,37 @@
 #include "movie.h"
 #include "parsing.h"
+#include "testing.h"
 #include <iostream>
 #include <unordered_map>
 #include <string>
 
-
-void printG(std::vector<Movie*>& g, int c) {
-    if (c == 0) {
-        return;
-    }
-    if (g.empty()) {
-        std::cout << "No movies with " << c << " genres in common." << std::endl;
-        return;
-    }
-    std::cout << "Movies with " << c << " genres in common" << std::endl;
-    for (const auto mov : g) {
-        Movie* movie = mov;
-        std::cout << "Name: " << movie->getName() << " | Rating: " << movie->getRating() << " | Genres: ";
-        for (auto gen : movie->getGenres()) {
-            std::cout << gen << " ,";
-        }
-        std::cout << std::endl;
-    }
-}
-
 int main() {
+    //basic structure/interface of the terminal:
 //     std::cout << "1. Enter First Movie Title" << std::endl;
 //     std::cout << "2. Enter Second Movie Title" << std::endl;
 //     std::cout << "3. Compute Data" << std::endl;
 //     std::cout << "4. Display Top Matches" << std::endl;
 //     std::cout << "5. Exit" << std::endl;
 
-    //testing purposes
+    //these maps are used for both approaches, important
     std::unordered_map<int, Movie> movieMap;
     std::unordered_map<std::string, Movie*> movieNames;
-    // std::string movieCSV = "test/movietest.csv";
-    // std::string ratingCSV = "test/ratingtest.csv";
 
     parseData(movieMap, movieNames);
 
-    // for (const auto& pair : movieNames) {
-    //     const Movie* movie = pair.second;
-    //     std::cout << "ID: " << movie->getId() << ", Title: " << pair.first << ", Average Rating: " << movie->getRating() << std::endl;
-    // } -- this works fine!
+    //testing purposes
+
     std::string mov1 = "The Matrix";
     std::string mov2 = "Inception";
-
     std::vector<std::pair<Movie*, int>> result = approach1(mov1, mov2, movieMap, movieNames);
 
-    // for (auto ele : result) {
-    //     std::cout << ele << " | " << std::endl;
-    // } -- this works fine!
     // std::vector<std::vector<Movie*>> g;
     std::vector<Movie*> g1, g2, g3;
+    std::unordered_map<int, std::vector<Movie*>> g;
 
     for (const auto& pair : result) {
+        int num = pair.second;
+
         if (pair.second == 1) {
             g1.push_back(pair.first);
         }
---
+++ testing.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include "movie.h"
+#include "testing.h"
+
+//test file used to see if the parsing works correctly
+
+//prints out the movie names
+void printMovieNames(std::unordered_map<std::string, Movie*> movieNames) {
+    for (const auto& pair : movieNames) {
+            const Movie* movie = pair.second;
+            std::cout << "ID: " << movie->getId() << ", Title: " << pair.first << ", Average Rating: " << movie->getRating() << std::endl;
+    }
+}
+
+//prints out approach 1 movies, not in order yet
+void printG(std::vector<Movie*>& g, int c) {
+    if (c == 0) {
+        return;
+    }
+    if (g.empty()) {
+        std::cout << "No movies with " << c << " genres in common." << std::endl;
+        return;
+    }
+    std::cout << "Movies with " << c << " genres in common" << std::endl;
+    for (const auto mov : g) {
+        Movie* movie = mov;
+        std::cout << "Name: " << movie->getName() << " | Rating: " << movie->getRating() << " | Genres: ";
+        for (auto gen : movie->getGenres()) {
+            std::cout << gen << " ,";
+        }
+        std::cout << std::endl;
+    }
+}
